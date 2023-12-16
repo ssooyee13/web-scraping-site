2929116218523988e7e490564589b750aaaf6bbd
ssooyee13 <78573112+ssooyee13@users.noreply.github.com>
2023-12-16 20:51:43 +0900

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
+from flask import Flask, render_template, request, redirect, send_file
 from indeed import extract_indeed_jobs
 from wwr import extract_wwr_jobs
 from file import save_to_file
 
-keyword = input("What do you want to search for?")
+app = Flask("JobScraper")
 
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-jobs = indeed + wwr
+db = {}
 
-save_to_file(keyword, jobs)
+
+@app.route("/")
+def home():
+    return render_template("home.html", name="sdawdsaw")
+
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
+
+
+app.run("0.0.0.0")
